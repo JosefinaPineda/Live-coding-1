3f6b793a94a20d8506184b019616120a9b7b078e
Josefina Pineda <jossepincor@gmail.com>
2024-07-19 11:21:45 -0600

    Update: ruta de archivos

---
+++ app.js
@@ -0,0 +1,8 @@
+// Escribe tu código aquí.
+/* Ejemplo:
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+bigWords('bocina', myArray);
+Output - ['insecto', 'bootcamp', 'escritorio'] */
+
+const myArray = ['azul', 'morado', 'verde', 'rojo', 'amarillo', 'rosa'];
+
